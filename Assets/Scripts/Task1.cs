991a85883c451df9a42a6cf62726b3ca80be9d27
cogito7 <galindom808@gmail.com>
2025-01-24 14:31:28 -0500

    Updates to Task1
    each factor calculates and adds to challenge score

--- Assets/Scripts/Task1.cs
+++ Assets/Scripts/Task1.cs
@@ -2,25 +2,46 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using System;
+
 
 public class Task1 : MonoBehaviour
 {
     public string courseName;
-    [Range(1, 10)] public int numberOfModules;
-    [Range(1, 10)] public int numberOfReadingMaterials;
-    [Range(1, 10)] public int numberOfQuizzes;
-    [Range(1, 10)] public int numberOfAssignments;
-    public bool isTheInstructorExperienced;
+    [Range(1, 10)] public int numModules;
+    [Range(1, 10)] public int numReadings;
+    [Range(1, 10)] public int numQuizzes;
+    [Range(1, 10)] public int numAssignments;
+    public bool isInstructorNonRookie;
 
-    private float courseChallenge;
+    private double challengeScore;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float modulePoints = (numberOfModules / 10) * 15;
-        courseChallenge = modulePoints;
-        Debug.Log(courseName + " has a difficulty rating of " + courseChallenge + "/10.");
+        //calculates each factor percentage
+        double modulePoints = ((double) numModules / 10) * 0.15;
+        challengeScore += modulePoints;
+
+        double readingsPoints = ((double) numReadings / 10) * 0.30;
+        challengeScore += readingsPoints;
+
+        double quizzesPoints = ((double) numQuizzes / 10) * 0.15;
+        challengeScore += quizzesPoints;
+
+        double assignmentsPoints = ((double)numAssignments / 10) * 0.30;
+        challengeScore += assignmentsPoints;
+
+        //adds points for experienced teacher
+        if (isInstructorNonRookie == true)
+        {
+            challengeScore += 0.10;
+        }
+
+        //outputs name and challengeScore
+        Debug.Log(courseName + " has a difficulty rating of " + challengeScore * 10 + "/10.");
     }
+
 
     // Update is called once per frame
     void Update()
